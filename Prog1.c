9b5091cf842cb2d3bd47b285447582e809e52594
anukritipandey6 <anukritipandey2404@gmail.com>
2025-11-30 22:43:06 +0530

    Q1.c

--- Prog1.c
+++ Prog1.c
@@ -1,19 +1,4 @@
 // Q1: Write a program to input two numbers and display their sum.
-
-/*
-Sample Test Cases:
-Input 1:
-3 4
-Output 1:
-Sum = 7
-
-Input 2:
--1 20
-Output 2:
-Sum = 19
-
-*/
-
 #include <stdio.h>
 
 int main()
@@ -32,4 +17,5 @@ int main()
     printf("Sum of %d and %d is: %d\n", a, b, sum);
 
     return 0;
+
 }
